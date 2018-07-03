4d9c5d804125d1f234c684ddf36027503064664c
lhx <Welcome1032@163.com>
2018-07-03 15:52:37 +0800

    vxnamedalaoban

---
+++ myfier/pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/img/xd_1.png',
+      '/img/xd_2.png',
+      '/img/xd_3.png'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  //事件处理函数
+  bindViewTap: function() {
+   
+  }
+})
